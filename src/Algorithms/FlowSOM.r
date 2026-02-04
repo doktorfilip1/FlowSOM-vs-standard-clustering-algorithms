46d88e8bf4ae56349087bd2653b7f5a383a50d07
Jovan <jovanrankovic77@gmail.com>
2026-02-04 16:41:13 +0100

    Finalno azuriranje kodova za sve standardne klastering algoritme i dodat R kod za FlowSOM algoritam kao i rezultati primene istog na sva 3 dataseta

--- src/Algorithms/FlowSOM.r
+++ src/Algorithms/FlowSOM.r
@@ -1,33 +1,100 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "ffb78745-5583-4e6a-bba8-ff5edf353df3",
-   "metadata": {},
-   "outputs": [],
-   "source": []
+library(FlowSOM)
+library(data.table)
+library(cluster)
+library(clusterSim)
+library(ggplot2)
+
+
+datasets <- list(
+  full = "../../data/processed/ProcessedData.csv",
+  pca  = "../../data/reduced/dataPCA.csv",
+  fs   = "../../data/reduced/dataFS.csv"
+)
+
+
+outDir <- "../../results/flowsom"
+
+
+xdim <- 10
+ydim <- 10
+metaClusters <- 4
+set.seed(42)
+
+
+results <- list()
+
+
+for (name in names(datasets)) {
+  
+  df <- fread(datasets[[name]])
+  if ("Class" %in% colnames(df)) {
+    df[, Class := NULL]
   }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.13.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
+  
+  X <- as.matrix(df)
+  
+  fsom <- FlowSOM(
+    X,
+    scale = FALSE,
+    colsToUse = 1:ncol(X),
+    xdim = xdim,
+    ydim = ydim,
+    nClus = metaClusters,
+    seed = 42
+  )
+  
+  
+  labels <- fsom$metaclustering[ fsom$map$mapping[,1] ]
+  labels <- as.integer(labels)
+  
+  
+  sil <- silhouette(labels, dist(X))
+  silScore <- mean(sil[, 3])
+  
+  dbScore <- index.DB(X, labels)$DB
+  
+  outClusters <- copy(df)
+  outClusters[, cluster := labels]
+  
+  fwrite(
+    outClusters,
+    file = paste0(outDir, "/flowsom_", name, "_clusters.csv")
+  )
+  
+
+  pca <- prcomp(X, center = TRUE, scale. = FALSE)
+  pcaData <- data.frame(
+    PC1 = pca$x[,1],
+    PC2 = pca$x[,2],
+    cluster = factor(labels)
+  )
+  
+  p <- ggplot(pcaData, aes(PC1, PC2, color = cluster)) +
+    geom_point(size = 0.6) +
+    theme_minimal() +
+    ggtitle(paste("FlowSOM (", name, ")", sep = ""))
+  
+  ggsave(
+    filename = paste0(outDir, "/flowsom_", name, ".png"),
+    plot = p,
+    width = 6,
+    height = 5,
+    dpi = 200
+  )
+  
+
+  results[[name]] <- data.table(
+    Dataset = name,
+    Algorithm = "FlowSOM",
+    SOM_grid = paste0(xdim, "x", ydim),
+    Meta_clusters = metaClusters,
+    Silhouette = silScore,
+    Davies_Bouldin = dbScore
+  )
 }
+
+
+summary <- rbindlist(results)
+fwrite(summary, paste0(outDir, "/flowsom_summary.csv"))
+
+print(summary)
